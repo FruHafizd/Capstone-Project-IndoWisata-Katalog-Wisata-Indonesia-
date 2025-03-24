90a08244551447e7daa6c7a6b082995c92b4b42e
FruHafizd <muhammadhafidz12333@gmail.com>
2025-03-25 05:53:18 +0800

    update users migration anda update form register

--- migrations/1742782615231_create-table-users.js
+++ migrations/1742782615231_create-table-users.js
@@ -1,31 +1,41 @@
 exports.up = (pgm) => {
-    pgm.createTable('users', {
-      id: {
-        type: 'VARCHAR(50)',
-        primaryKey: true,
-      },
-      name: {
-        type: 'VARCHAR(100)',
-        notNull: true,
-      },
-      email: {
-        type: 'VARCHAR(100)',
-        notNull: true,
-        unique: true,
-      },
-      password: {
-        type: 'VARCHAR(255)',
-        notNull: true,
-      },
-      role: {
-        type: 'VARCHAR(20)',
-        notNull: true,
-        default: "'user'", // default role adalah 'user'
-      },
-    });
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    nama: {
+      type: 'VARCHAR(100)',
+      notNull: true,
+    },
+    email: {
+      type: 'VARCHAR(100)',
+      notNull: true,
+      unique: true,
+    },
+    password: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    hobby: {
+      type: 'TEXT',
+      notNull: false,
+    },
+    usia: {
+      type: 'INTEGER',
+      notNull: false,
+    },
+    pekerjaan: {
+      type: 'VARCHAR(100)',
+      notNull: false,
+    },
+    status_nikah: {
+      type: 'VARCHAR(20)',
+      notNull: false,
+    },
+  });
 };
-  
+
 exports.down = (pgm) => {
-    pgm.dropTable('users');
+  pgm.dropTable('users');
 };
-  
