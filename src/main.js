887cc232884554954d39702dac9bfdf94530e3c2
FruHafizd <muhammadhafidz12333@gmail.com>
2025-03-26 06:05:22 +0800

    Fixing Category dll

---
+++ src/main.js
@@ -0,0 +1,22 @@
+
+import initCategory from './frontend/views/category.js';
+import initHome from './frontend/views/home.js';
+import initSearch from './frontend/views/search.js';
+import initDetail from './frontend/views/detail.js';
+import './frontend/js/login.js';
+import './frontend/js/register.js';
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (window.location.pathname.includes("category.html")) {
+        initCategory();
+    } else if (window.location.pathname.includes("search.html")) {
+        initSearch();
+    } else if (window.location.pathname.includes("detail.html")) {
+        initDetail();
+    } else if (
+      window.location.pathname.includes("index.html") ||
+      window.location.pathname === "/"  // untuk kasus URL root
+    ) {
+        initHome();
+    }
+});
